515ba0063790e3604ff6563e0a91a90b1bc81dab
Eros Jose Adarraga Jimenez <dev.erosadarraga@gmail.com>
2023-04-16 12:09:46 -0500

    Implementando los controladores y clases para la aplicacion

--- UniversityApiBackend/Models/DataModels/BaseEntity.cs
+++ UniversityApiBackend/Models/DataModels/BaseEntity.cs
@@ -8,7 +8,7 @@ namespace UniversityApiBackend.Models.DataModels
         [Key]
         public int Id { get; set; }
         public string CreatedBy { get; set; }= string.Empty;
-        public DateTime CreatedAt { get; set; }
+        public DateTime CreatedAt { get; set; }= DateTime.Now;
         public string UpdatedBy { get; set; } = string.Empty;
         public DateTime? UpdatedAt { get; set; }
         public string DeletedBy { get; set; } = string.Empty;
