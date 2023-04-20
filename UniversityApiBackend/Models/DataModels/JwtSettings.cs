b5bdd56ed7fefaf58ec05e565e5ee10bc5ea88e4
Eros Jose Adarraga Jimenez <dev.erosadarraga@gmail.com>
2023-04-20 14:51:31 -0500

    Creacion de Claims

---
+++ UniversityApiBackend/Models/DataModels/JwtSettings.cs
@@ -0,0 +1,20 @@
+﻿namespace UniversityApiBackend.Models.DataModels
+{
+    public class JwtSettings
+    {
+        public bool validateIsUserSingingKey { get; set; }
+        public string? IsUserSigningKey { get; set; }
+
+        public bool validateIsUser { get; set; } = true;
+        public string? ValidIsUser { get; set; }
+
+        public bool ValidateAudience { get; set; } = true;
+        public string? ValidAudience { get; set;}
+
+        public bool RequireExpirationTime { get; set; }
+        public bool ValidateLifeTime { get; set; }= true;
+         
+        
+    }
+}
+ 
