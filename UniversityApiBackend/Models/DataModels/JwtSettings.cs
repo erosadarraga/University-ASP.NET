f97f43bc6ccf3b10d721abdc4b1e5e23fbe63b0b
Eros Jose Adarraga Jimenez <dev.erosadarraga@gmail.com>
2023-04-20 18:21:27 -0500

    Controler de ruta privada

--- UniversityApiBackend/Models/DataModels/JwtSettings.cs
+++ UniversityApiBackend/Models/DataModels/JwtSettings.cs
@@ -3,7 +3,7 @@
     public class JwtSettings
     {
         public bool validateIsUserSingingKey { get; set; }
-        public string? IsUserSigningKey { get; set; }
+        public string IsUserSigningKey { get; set; } = string.Empty;
 
         public bool validateIsUser { get; set; } = true;
         public string? ValidIsUser { get; set; }
