e2a458eeeffb6c7226852739b0a122c9bdb96925
Eros Jose Adarraga Jimenez <dev.erosadarraga@gmail.com>
2023-04-20 08:35:28 -0500

    Agregando politicas de Cors y services students

--- LinqSnippets/Snippets.cs
+++ LinqSnippets/Snippets.cs
@@ -6,8 +6,44 @@ using System.Text;
 
 namespace LinqSnippets
 {
+
+
+
     public class Snippets
     {
+
+        static public void BasicLinQ()
+        {
+
+            string[] cars =
+            {
+                "VW Golf",
+                "VW California",
+                "Audi A3",
+                "Audi A5",
+                "Fiat Punto",
+                "Seat Ibiza",
+                "Seat leon"
+            };
+
+            // 1. SELECT * of cars
+
+            var carList = from car in cars select car;
+
+            foreach ( var car in carList )
+            {
+                Console.WriteLine( car );
+            }
+            // 2. SELECT WHERE car is Audi
+            var AudiList = from car in cars where car.Contains("Audi") select car;
+
+            foreach ( var car in AudiList )
+            {
+                Console.WriteLine( car );
+            }
+
+
+        }
 
     }
 }
